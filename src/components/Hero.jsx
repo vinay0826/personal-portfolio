74c6c997757a3ac10d3496c3c90e1907adcae0a7
Vinay Nalagatla <158601743+vinay0826@users.noreply.github.com>
2025-05-03 20:45:29 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -149,7 +149,7 @@ const Hero = () => {
         </div>
       </div>
       <h1 className='special-font hero-heading absolute bottom-5 right-5 text-black'>
-        G<b>A</b>MING
+        
       </h1>
     </div>
   );
